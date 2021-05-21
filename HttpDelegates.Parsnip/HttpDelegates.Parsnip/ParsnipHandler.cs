ae12f652c3d82b715d46dee9a7a677c3f4055023
Matt Morden <Mattymorden@gmail.com>
2021-05-21 16:38:12 -0400

    Adding ParsnipHandler

---
+++ HttpDelegates.Parsnip/HttpDelegates.Parsnip/ParsnipHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpDelegates.Parsnip
+{
+    public class ParsnipHandler : DelegatingHandler
+    {       
+        public ParsnipHandler() { }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            AddParsnipHeader(request);
+            return await base.SendAsync(request, cancellationToken);
+        }
+
+        private void AddParsnipHeader(HttpRequestMessage request)
+        {
+            if (request.Headers.Contains("Parsnip"))
+            {
+                request.Headers.Remove("Parsnip");
+            }
+            request.Headers.Add("Parsnip", "YesPlease");
+        }
+    }
+}
